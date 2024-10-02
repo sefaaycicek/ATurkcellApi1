c14dbf4648f4f840a894b844ea8c84a63d54560d
Sefa Aycicek <saycicek@ipucubilisim.com.tr>
2024-10-02 13:36:03 +0300

    Initial Commit

---
+++ ATurkcellApi1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ATurkcellApi1
+//
+//  Created by Sefa Aycicek on 2.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
